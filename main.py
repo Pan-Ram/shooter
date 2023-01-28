91c551f8327f60248f58d926e1c58006e1fbc03a
Timur Ostapets <timurostapets@gmail.com>
2023-01-28 11:03:09 +0200

    added class Player + created it

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def file_path(file_name):
     path = os.path.join(folder_path, file_name)
     return path
 
-FPS = 40
+FPS = 60
 WIN_WIDTH = 700
 WIN_HEIGHT = 500
 
@@ -18,16 +18,31 @@ background = pygame.image.load(file_path("background.png"))
 background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
 
 class GameSprite(pygame.sprite.Sprite):
-    def __init__(self, sprite_image, sprite_x, sprite_y, sprite_speed, sprite_width, sprite_height):
+    def __init__(self, image, x, y, speed, width, height):
         super().__init__()
-        self.image = pygame.image.load(file_path(sprite_image))
-        self.image = pygame.transform.scale(self.image, (sprite_width, sprite_height))
+        self.image = pygame.image.load(file_path(image))
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect()
-        self.rect.x = sprite_x
-        self.rect.y = sprite_y
-        self.speed = sprite_speed
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def __init__(self, image, x, y, speed, width, height):
+        super().__init__(image, x, y, speed, width, height)
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+    
+    def fire(self):
+        pass
+
+player = Player("dog.png", 300, 375, 5, 65, 65)
 
 play = True
 game = True
@@ -42,6 +57,9 @@ while game == True:
 
     if play == True:
         window.blit(background, (0, 0))
+
+        player.reset()
+        player.update()
 
     clock.tick(FPS)
     pygame.display.update()
